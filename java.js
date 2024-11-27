c78d8622ecf120a2e9713fc23a4988e63fbb3f75
Tasneem Shaaban <tasneemmshaban88@gmail.com>
2024-11-27 04:40:20 +0200

    Create java.js

---
+++ java.js
@@ -0,0 +1,12 @@
+const header =document.querySelector("header");
+window.addEventListener("scroll",function(){
+    header.classList.toggle("sticky",this.window.scrollY>0)
+})
+
+let menu = document.querySelector('#menu-icon');
+let nav = document.querySelector('.nav-menu');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    nav.classList.toggle('open');
+}
